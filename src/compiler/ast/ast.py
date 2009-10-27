b508cbf84132a024d21cabc6de297654c4be7f6e
cirodrig <cirodrig@crhc.illinois.edu>
2009-10-27 16:24:44 -0500

    Changed argument order of Expr.__init__()
    darcs-hash:20091027212444-8b57f-2f48d0a799936341c038118f1da5995a86fdbc8f.gz

--- src/compiler/ast/ast.py
+++ src/compiler/ast/ast.py
@@ -37,7 +37,8 @@ class ExprInit(object):
         This is called from the expression's __init__ method."""
         return ()
 
-exprDefault = ExprInit() 
+# The default value of ExprInit
+ExprInit.default = ExprInit() 
 
 class Expr(object):
     """An abstract base class of expressions."""
@@ -48,8 +49,8 @@ class Expr(object):
 class VariableExpr(Expr):
     """A reference to a variable"""
     
-    def __init__(self, initobj, v):
-        Expr.__init__(self, initobj)
+    def __init__(self, v, base = ExprInit.default):
+        Expr.__init__(self, base)
         self.variable = v
 
 class LiteralExpr(Expr):
@@ -57,15 +58,15 @@ class LiteralExpr(Expr):
 
     Literals can be numbers, booleans, or the None value."""
 
-    def __init__(self, initobj, l):
-        Expr.__init__(self, initobj)
+    def __init__(self, l, base = ExprInit.default):
+        Expr.__init__(self, base)
         self.literal = l
 
 class UnaryExpr(Expr):
     """An application of a unary operator to an operand."""
 
-    def __init__(self, initobj, op, arg):
-        Expr.__init__(self, initobj)
+    def __init__(self, op, arg, base = ExprInit.default):
+        Expr.__init__(self, base)
         assert isinstance(op, operators.UnaryOp)
         assert isinstance(arg, Expr)
         self.operator = op
@@ -74,8 +75,8 @@ class UnaryExpr(Expr):
 class BinaryExpr(Expr):
     """An application of a binary operator to left and right operands."""
 
-    def __init__(self, initobj, op, left, right):
-        Expr.__init__(self, initobj)
+    def __init__(self, op, left, right, base = ExprInit.default):
+        Expr.__init__(self, base)
         assert isinstance(op, operators.BinaryOp)
         assert isinstance(left, Expr)
         assert isinstance(right, Expr)
@@ -85,8 +86,8 @@ class BinaryExpr(Expr):
 
 class LetExpr(Expr):
     """An assignment expression"""
-    def __init__(self, initobj, name, rhs, body):
-        Expr.__init__(self, initobj)
+    def __init__(self, name, rhs, body, base = ExprInit.default):
+        Expr.__init__(self, base)
         assert isinstance(name, Variable)
         assert isinstance(rhs, Expr)
         assert isinstance(body, Expr)
@@ -96,8 +97,8 @@ class LetExpr(Expr):
 
 class LetrecExpr(Expr):
     """A set of function definitions"""
-    def __init__(self, initobj, definitions, body):
-        Expr.__init__(self, initobj)
+    def __init__(self, definitions, body, base = ExprInit.default):
+        Expr.__init__(self, base)
         for d in definitions: assert isinstance(d, FunctionDef)
         assert isinstance(body, Expr)
         self.definitions = definitions
--- src/compiler/example_ast.py
+++ src/compiler/example_ast.py
@@ -6,12 +6,15 @@ b_var = PythonVariable("b")
 c_var = PythonVariable("c")
 f_var = PythonVariable("f")
 
+# def f(a, b):
+#     c = a + b
+#     c
+
 f = FunctionDef(f_var,
                 Function([a_var, b_var],
-                         LetExpr(exprDefault,
-                                 c_var,
-                                 BinaryExpr(exprDefault,
-                                            operators.ADD, VariableExpr(exprDefault, a_var), VariableExpr(exprDefault, b_var)),
-                                 VariableExpr(exprDefault,
-                                              c_var))))
+                         LetExpr(c_var,
+                                 BinaryExpr(operators.ADD,
+                                            VariableExpr(a_var),
+                                            VariableExpr(b_var)),
+                                 VariableExpr(c_var))))
 
