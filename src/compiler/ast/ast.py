5254f200e6973fbc5bbc2fce9d950f62f0809c1c
cirodrig <cirodrig@crhc.illinois.edu>
2009-10-28 13:22:01 -0500

    Added more Python expression types
    darcs-hash:20091028182201-8b57f-e5c31a45ef445bf65445c8bd6356e7fa2a14ed1b.gz

--- src/compiler/ast/ast.py
+++ src/compiler/ast/ast.py
@@ -13,12 +13,14 @@ class Variable(object):
     def __init__(self):
         raise NotImplementedError, "'Variable' is an abstract base class"
 
+    def __eq__(self, other):
+        raise NotImplementedError, "'Variable' is an abstract base class"
+
 class PythonVariable(Variable):
     """A variable as determined by Python's name resolution rules"""
 
     def __init__(self, name):
         self.name = name
-        
 
 ###############################################################################
 # Function parameters
@@ -66,80 +68,110 @@ class ExprInit(object):
 # The default value of ExprInit
 ExprInit.default = ExprInit() 
 
-class Expr(object):
+class Expression(object):
     """An abstract base class of expressions."""
 
     def __init__(self, arg):
         arg.initializeExpr(self)
 
-class VariableExpr(Expr):
+class VariableExpr(Expression):
     """A reference to a variable"""
     
     def __init__(self, v, base = ExprInit.default):
-        Expr.__init__(self, base)
+        Expression.__init__(self, base)
         self.variable = v
 
-class LiteralExpr(Expr):
+class LiteralExpr(Expression):
     """A reference to a primitive, immutable literal value.
 
     Literals can be numbers, booleans, or the None value."""
 
     def __init__(self, l, base = ExprInit.default):
-        Expr.__init__(self, base)
+        Expression.__init__(self, base)
         self.literal = l
 
-class UnaryExpr(Expr):
+class UnaryExpr(Expression):
     """An application of a unary operator to an operand."""
 
     def __init__(self, op, arg, base = ExprInit.default):
-        Expr.__init__(self, base)
+        Expression.__init__(self, base)
         assert isinstance(op, operators.UnaryOp)
-        assert isinstance(arg, Expr)
+        assert isinstance(arg, Expression)
         self.operator = op
         self.argument = arg
 
-class BinaryExpr(Expr):
+class BinaryExpr(Expression):
     """An application of a binary operator to left and right operands."""
 
     def __init__(self, op, left, right, base = ExprInit.default):
-        Expr.__init__(self, base)
+        Expression.__init__(self, base)
         assert isinstance(op, operators.BinaryOp)
-        assert isinstance(left, Expr)
-        assert isinstance(right, Expr)
+        assert isinstance(left, Expression)
+        assert isinstance(right, Expression)
         self.operator = op
         self.left = left
         self.right = right
 
-class CallExpr(Expr):
+class IfExpr(Expression):
+    """An if-else expression."""
+
+    def __init__(self, argument, ifTrue, ifFalse, base = ExprInit.default):
+        Expression.__init__(self, base)
+        assert isinstance(test, Expression)
+        assert isinstance(ifTrue, Expression)
+        assert isinstance(ifFalse, Expression)
+        self.argument = argument
+        self.ifTrue = ifTrue
+        self.ifFalse = ifFalse
+
+class ForExpr(Expression):
+    """A 'for' loop or generator expression."""
+
+    def __init__(self, param, argument, body, base = ExprInit.default):
+        assert isinstance(param, Parameter)
+        assert isinstance(argument, Expression)
+        assert isinstance(body, Expression)
+        self.parameter = param
+        self.argument = argument
+        self.body = body
+
+class CallExpr(Expression):
     """A function call."""
 
     def __init__(self, operator, arguments, base = ExprInit.default):
-        Expr.__init__(self, base)
-        assert isinstance(operator, Expr)
+        Expression.__init__(self, base)
+        assert isinstance(operator, Expression)
         for arg in arguments:
-            assert isinstance(arg, Expr)
+            assert isinstance(arg, Expression)
         self.operator = operator
         self.arguments = arguments
 
-class LetExpr(Expr):
+class LetExpr(Expression):
     """An assignment expression"""
     def __init__(self, lhs, rhs, body, base = ExprInit.default):
-        Expr.__init__(self, base)
+        Expression.__init__(self, base)
         assert lhs is None or isinstance(lhs, Parameter)
-        assert isinstance(rhs, Expr)
-        assert isinstance(body, Expr)
+        assert isinstance(rhs, Expression)
+        assert isinstance(body, Expression)
         self.name = lhs
         self.rhs = rhs
         self.body = body
 
-class LetrecExpr(Expr):
+class LetrecExpr(Expression):
     """A set of function definitions"""
     def __init__(self, definitions, body, base = ExprInit.default):
-        Expr.__init__(self, base)
+        Expression.__init__(self, base)
         for d in definitions: assert isinstance(d, FunctionDef)
-        assert isinstance(body, Expr)
+        assert isinstance(body, Expression)
         self.definitions = definitions
         self.body = body
+
+class FunExpr(Expression):
+    """A lambda function"""
+    def __init__(self, function, base = ExprInit.default):
+        Expression.__init__(self, base)
+        assert isinstance(function, Function)
+        self.function = function
 
 ###############################################################################
 # Functions
@@ -157,7 +189,7 @@ class Function(object):
     def __init__(self, parameters, body):
         for p in parameters:
             assert isinstance(p, Parameter)
-        assert isinstance(body, Expr)
+        assert isinstance(body, Expression)
         self.parameters = parameters
         self.body = body
 
--- src/compiler/example_ast.py
+++ src/compiler/example_ast.py
@@ -1,4 +1,6 @@
 
+# This file contains some handwritten example AST code
+
 from ast import *
 
 a_var = PythonVariable("a")
@@ -17,4 +19,28 @@ f = FunctionDef(f_var,
                                             VariableExpr(a_var),
                                             VariableExpr(b_var)),
                                  VariableExpr(c_var))))
+
+xs_var = PythonVariable("xs")
+ys_var = PythonVariable("ys")
+x_var = PythonVariable("x")
+y_var = PythonVariable("y")
+nn_var = PythonVariable("nn")
+
+# lambda x: dist(x,y)
+nn_lambda = Function([VariableParam(x_var)],
+                     CallExpr(VariableExpr(PythonVariable("dist")),
+                              [VariableExpr(x_var), VariableExpr(y_var)]))
+
+# [minIndex(lambda x: dist(x,y), ys) for x in xs]
+nn_body = ForExpr(VariableParam(x_var),
+                  VariableExpr(xs_var),
+                  CallExpr(VariableExpr(PythonVariable("minIndex")),
+                           [FunExpr(nn_lambda), VariableExpr(ys_var)]))
+
+# def nn(xs, ys):
+#     return [minIndex(lambda x: dist(x,y), ys) for x in xs]
+
+nn = FunctionDef(nn_var,
+                 Function([VariableParam(xs_var), VariableParam(ys_var)],
+                          nn_body))
 
