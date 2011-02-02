5d9e162d29da1e14b01eaf5f275bdef452667bd6
wentz1 <wentz1@illinois.edu>
2011-02-02 15:36:29 -0600

    Separated into Restructure and Traversal functions
    Ignore-this: 9ba06c2d7485fe5b9b3b95f7c57e8290
    
    Also removed remnants of "Case" handling
    
    darcs-hash:20110202213629-abd5b-15a983c226cbffb5bd8cee39b09b77a71dfcea10.gz

--- src/program/SystemF/LocalExprRewrite.hs
+++ src/program/SystemF/LocalExprRewrite.hs
@@ -38,23 +38,8 @@ data LetPart s = LetPart { lpInfo :: ExpInfo
                          , lpValue :: Exp s
                          -- lpBody :: Exp s
                          }
-
-data AltPart s = AltPart
-                 { altpConstructor :: !Var
-                 , altpTyArgs      :: [Typ s]
-                 , altpExTypes     :: [TyPat s]
-                 , altpParams      :: [Pat s]
-                 -- altpBody        :: Exp s
-                 }            
-                         
-data CasePart s = CasePart 
-                  { cpInfo :: ExpInfo
-                  , cpScrutinee :: Exp s
-                  , cpAltPart :: AltPart s
-                  }
                   
-type LetPartM = LetPart Mem
-type CasePartM = CasePart Mem                  
+type LetPartM = LetPart Mem              
                                                    
 
 shapeLet :: ExpM -> LetPartM -> LR ExpM
@@ -63,71 +48,41 @@ shapeLet body (LetPart lpInf lpBind lpVal) = do
   renamed <- freshen reshaped
   return renamed
   
-  {-
-shapeCase :: ExpM -> CasePartM -> LR ExpM
-shapeCase body (CasePart cpInf cpScrut cpAltPart) = do
-  case cpAltPart of
-    AltPart const tyArgs exTypes params -> do
-      let fullAlt = AltM $ Alt const tyArgs exTypes params body
- --     renamed <- freshen fullAlt
-      return $ ExpM $ CaseE cpInf cpScrut [fullAlt]
-  -}
 constructLet :: ExpM -> [LetPartM] -> LR ExpM
 constructLet body [] = return body
 constructLet body parts = do
   result <- foldM shapeLet body parts
   return result
-  
-  {-
-constructCase :: ExpM -> [CasePartM] -> LR ExpM
-constructCase body [] = return body
-constructCase body parts = do
-  result <- foldM shapeCase body parts
-  return result
--}
-
 
 rwAlt :: AltM -> LR AltM
 rwAlt (AltM (Alt const tyArgs exTypes params body)) = do
-  body' <- topExp body
+  body' <- rwExp body
   return $ AltM $ Alt const tyArgs exTypes params body'
      
-delveExp :: ExpM -> LR ([LetPartM], [CasePartM], ExpM)
+delveExp :: ExpM -> LR ([LetPartM], ExpM)
 delveExp (ExpM ex) = do
   case ex of
     LetE inf bind val body -> do
-      body' <- topExp body
-      (letPs, casePs, replaceExp) <- delveExp val
+      body' <- rwExp body
+      (letPs, replaceExp) <- delveExp val
       case bind of
         MemVarP var ptype -> do
           let letPart = LetPart inf bind replaceExp
-          {-trace "Found a Let MemVar, lifting..."-} 
-          return ( letPart:letPs , casePs, body')
+          return ( letPart:letPs , body')
         _ -> do
           let result = ExpM $ LetE inf bind replaceExp body'
-          {-trace "Found a Let LocalVar, returning..."-} 
-          return (letPs, casePs, result)
- {-   CaseE inf scrut ((AltM alt):[]) -> do
-      scrut' <- topExp scrut
-      case alt of
-        Alt const tyArgs exTypes params body -> do
-          (letPs, casePs, replaceExp) <- delveExp body
-          let altPart = AltPart const tyArgs exTypes params
-          let casePart = CasePart inf scrut' altPart
-          {-trace ("Lifting Case, to return: "++(show (List.length letPs))++" lets, "++(show (1+(List.length casePs)))++" cases")-} 
-          return (letPs, casePart:casePs, replaceExp)
-          -}
+          return (letPs, result)
     AppE inf oper tyArgs args -> do
       tupList <- mapM delveExp args
-      let (letParts, caseParts, toReplaceArgs) = unzip3 tupList
+      let (letParts, toReplaceArgs) = unzip tupList
       let letParts' = concat letParts
-      let caseParts' = concat caseParts
       let replacedApp = ExpM $ AppE inf oper tyArgs toReplaceArgs
-      return (letParts', caseParts', replacedApp)
-    _ -> do -- includes multiple alternative cases
-      ex' <- {-trace "Other type in delveExp, returning..."-} topExp (ExpM ex)
-      return ([], [], ex')    
+      return (letParts', replacedApp)
+    _ -> do
+      ex' <- rwExp (ExpM ex)
+      return ([], ex')    
 
+{-
 topExp :: ExpM -> LR ExpM
 topExp (ExpM ex) = do
   case ex of
@@ -156,10 +111,50 @@ topExp (ExpM ex) = do
     LamE inf fun -> do fun' <- rwFun fun
                        return $ ExpM $ LamE inf fun'
     _ -> return $ ExpM ex -- Var and Lit
+    -}
+    
+restructureExp :: ExpM -> LR ExpM
+restructureExp (ExpM ex) = do
+  case ex of
+    LetE inf bind val body -> do
+      body' <- rwExp body
+      (letPs, val') <- {-trace "Done with body, moving to Val"-} delveExp val
+      let replaced = ExpM $ LetE inf bind val' body'
+      postLet <- constructLet replaced letPs
+      return postLet
+    AppE inf oper tyArgs args -> do
+      oper' <- rwExp oper
+      tupList <- mapM delveExp args
+      let (letParts, toReplaceArgs) = unzip tupList
+      let letParts' = concat letParts
+      let replacedApp = ExpM $ AppE inf oper' tyArgs toReplaceArgs
+      afterLetParts <- constructLet replacedApp letParts'
+      return afterLetParts
+    _ -> return $ ExpM ex
+    
+rwExp :: ExpM -> LR ExpM
+rwExp (ExpM ex) = do
+  case ex of
+    LetE inf bind val body -> do
+      ex' <- restructureExp (ExpM ex)
+      return $ ex'
+    CaseE inf scrut alts -> do scrut' <- rwExp scrut
+                               alts' <- mapM rwAlt alts
+                               return $ ExpM $ CaseE inf scrut' alts'
+    AppE inf oper tyArgs args -> do
+      ex' <- restructureExp (ExpM ex)
+      return $ ex'
+    LetrecE inf defs body -> do defs' <- mapM rwDef defs
+                                body' <- rwExp body
+                                return $ ExpM $ LetrecE inf defs' body'
+    LamE inf fun -> do fun' <- rwFun fun
+                       return $ ExpM $ LamE inf fun'
+    _ -> return $ ExpM ex -- Var and Lit
+    
     
 rwFun :: FunM -> LR FunM
 rwFun (FunM f) = do
-  body' <- {-trace "in rwFun"-} topExp (funBody f)
+  body' <- rwExp (funBody f)
   return $ FunM $ Fun { funInfo = funInfo f
                       , funTyParams = funTyParams f
                       , funParams = funParams f
