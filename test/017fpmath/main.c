6327afaf75b76d68677de720bc3deddc8f3ddbf1
cirodrig <cirodrig@illinois.edu>
2011-02-14 13:58:55 -0600

    Test case for floating-point math
    Ignore-this: 436f3795156b715edababcffca9bc1c2
    
    darcs-hash:20110214195855-11156-076e264c0704794060e3691899e20ddba44a35df.gz

---
+++ test/017fpmath/main.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+#include "fp_interface.h"
+
+int main()
+{
+  float x = f(77.77);		/* Result is 0.5 * 77.77 */
+  float y = g(5.0/9.0);		/* Result is 1 */
+
+  if (x < 38.8 || x > 38.9)
+    printf ("Wrong x: %f", x);
+  else if (y < 0.99 || y > 1.01)
+    printf("Wrong y: %f", y);
+  else
+    printf("ok");
+
+  return 0;
+}
