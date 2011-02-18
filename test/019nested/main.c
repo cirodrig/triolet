8e2d1f2b0751864ab99176687eff5d04f5847975
cirodrig <cirodrig@illinois.edu>
2011-02-18 16:55:15 -0600

    New test case for nested loops
    Ignore-this: d134aac4d31c53e9c1bb65e7974b5cb1
    
    darcs-hash:20110218225515-11156-e87d1d57146cc7cf2631210eb13b61fcdf895713.gz

---
+++ test/019nested/main.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+#include "nested_interface.h"
+
+// Some random numbers.
+PyonFloat xs[] = {5.7, 0.5, 0.8, 2.4, 2.2, 3.4};
+PyonFloat ys[] = {5.9, 2.3, 4.3, 3.8, 3.4, 2.3};
+
+int main()
+{
+  PyonList *pxs = pyon_List_PyonFloat_FromArray(6, xs);
+  PyonList *pys = pyon_List_PyonFloat_FromArray(6, ys);
+
+  // Compute sum [sin(x) * sin(y) | x <- xs, y <- ys]
+  PyonFloat f = nred(pxs, pys);
+
+  pyon_List_PyonInt_Free(pxs);
+  pyon_List_PyonInt_Free(pys);
+
+  if (f >= -1.24849 && f <= -1.24848)
+    printf("ok");
+  else
+    printf("not ok");
+  
+  return 0;
+}
