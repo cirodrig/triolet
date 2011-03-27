3ab5684fd7da9594256a191e61ba7eac68b40a52
cirodrig <cirodrig@illinois.edu>
2011-03-27 16:56:07 -0500

    New test case 026pairwiseB
    Ignore-this: 289da6de55b8a0e9aa7c372246d9d43f
    
    This test case currently fails during type inference
    
    darcs-hash:20110327215607-11156-ca03f1f468ed3484feebe2298aa8edf267314bd1.gz

---
+++ test/026pairwiseB/main.c
@@ -0,0 +1,61 @@
+
+#include <stdio.h>
+#include "pairwise_interface.h"
+
+// Random points in a unit 3-ball
+PyonFloat x_coords[10] = {
+  8.860380111462525e-2,
+  -7.77867287578496e-2,
+  0.547480095305359,
+  9.957931286179911e-2,
+  -0.3601525822561589,
+  -9.411760561496894e-2,
+  -0.748423282696964,
+  0.14766362685422962,
+  0.7937266921609936,
+  -0.373228905809398
+};
+
+PyonFloat y_coords[10] = {
+  -0.4240137931015375,
+  -9.498282803571383e-3,
+  0.6771305842970337,
+  0.4516891814888663,
+  -0.7275522660295368,
+  -0.2252140963043119,
+  -1.5683518726304993e-4,
+  -0.24412661330870505,
+  0.28657486389544207,
+  0.46424868573999234
+};
+
+PyonFloat z_coords[10] = {
+  -0.29495616450322704,
+  0.2366519985340191,
+  0.20904609193304696,
+  0.6872986002562798,
+  -3.0957500923182234e-2,
+  -0.5759434617534148,
+  -7.419078985093878e-2,
+  0.9431492134330676,
+  -0.24337809631679627,
+  0.6533732220189118
+};
+
+int main() {
+  Pyon_init();
+  PyonList *x_list = pyon_List_PyonFloat_FromArray(10, x_coords);
+  PyonList *y_list = pyon_List_PyonFloat_FromArray(10, y_coords);
+  PyonList *z_list = pyon_List_PyonFloat_FromArray(10, z_coords);
+
+  PyonFloat energy = compute_energy(x_list, y_list, z_list);
+  pyon_List_PyonFloat_Free(x_list);
+  pyon_List_PyonFloat_Free(y_list);
+  pyon_List_PyonFloat_Free(z_list);
+
+  if (energy < 56.59 || energy > 56.60)
+    printf("not ok");
+  else
+    printf("ok");
+  return 0;
+}
