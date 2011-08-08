f00d18e2f4de403e05042973358c1f7fd8a618ad
cirodrig <cirodrig@illinois.edu>
2011-08-08 12:15:47 -0500

    New test case 034stencil
    Ignore-this: cb98b526bb5b98aa2ad3f228ce6f73c7
    
    darcs-hash:20110808171547-11156-5aee388fe13154557fece64269842e639393c80a.gz

---
+++ test/034stencil/main.c
@@ -0,0 +1,56 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pyon.h>
+#include <math.h>
+#include "stencil_interface.h"
+
+#define DIMY 5
+#define DIMX 4
+
+float arr[] = {
+  0.4,   0, 0,   0.4,
+  0,   0, 0.8,   0,
+  0,   0, 0,   0,
+  0,   0, 0,   0,
+  0.4,   0, 0,   0.4
+};
+
+float expected_result[] = {
+  0.2, 0.1, 0.3, 0.2,
+  0.1, 0.2, 0,   0.3,
+  0,   0,   0.2, 0,
+  0.1, 0,   0,   0.1,
+  0.2, 0.1, 0.1, 0.2
+};
+
+int main()
+{
+  Pyon_init();
+
+  PyonMatrix *mat1 = pyon_Matrix_PyonFloat_FromArray(DIMY, DIMX, arr);
+  PyonMatrix *mat2 = heat(mat1);
+  pyon_Matrix_PyonFloat_Free(mat1);
+
+  // Verify size of output
+  if (pyon_Matrix_PyonFloat_Height(mat2) != DIMY) return -1;
+  if (pyon_Matrix_PyonFloat_Width(mat2) != DIMX) return -1;
+
+  // Get matrix data
+  float *data = malloc(DIMY * DIMX * sizeof(int));
+  pyon_Matrix_PyonFloat_ToArray(mat2, data);
+  pyon_Matrix_PyonFloat_Free(mat2);
+
+  // Compare against expected output
+  int y, x;
+  for (y = 0; y < DIMY; y++) {
+    for (x = 0; x < DIMX; x++) {
+      if (fabs(data[y * DIMX + x] - expected_result[y * DIMX + x]) > 1e-5) {
+        printf("Not OK");
+        return 0;
+      }
+    }
+  }
+  printf("OK");
+  return 0;
+}
