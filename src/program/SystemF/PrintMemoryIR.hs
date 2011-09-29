ea79487687d7489ac3aaf56143db4d0e79b06d3e
cirodrig <cirodrig@illinois.edu>
2011-09-29 12:43:19 -0500

    More consistent type application syntax and reduced indentation in core printer
    Ignore-this: 59a4d9bddee5728ab6aea1e1ea7cd7f
    
    darcs-hash:20110929174319-11156-b11bd63b9548da1dd27fdb77d2ad51e31a50c30f.gz

--- src/program/SystemF/PrintMemoryIR.hs
+++ src/program/SystemF/PrintMemoryIR.hs
@@ -1,5 +1,21 @@
 
-module SystemF.PrintMemoryIR where
+module SystemF.PrintMemoryIR
+       (pprLit,
+        pprDmd,
+        pprSpecificity,
+        pprTypM,
+        pprTyPat,
+        pprPat,
+        pprExp,
+        pprExpPrec,
+        pprAlt,
+        pprFun,
+        pprFunPrec,
+        pprDef,
+        pprExport,
+        pprModule
+       )
+where
 
 import Text.PrettyPrint.HughesPJ
 
@@ -10,7 +26,18 @@ import SystemF.Syntax
 import SystemF.MemoryIR
 import Type.Type
 
+defIndent = 2
+letIndent = 2
+appIndent = 4
+caseIndent = 6
+
 pprParenList xs = parens $ sep $ punctuate (text ",") xs
+
+pprTyArgs ts = [text "@" <> pprTypePrec t ?+ appPrec | t <- ts]
+pprExTypeArgs ts = [text "&" <> pprTypePrec t ?+ appPrec | t <- ts]
+pprExTypeBinders ts = [text "&" <> parens (pprVar v <+> text ":" <+> pprType t)
+                      | (v ::: t) <- ts]
+
 
 pprLit :: Lit -> Doc
 pprLit (IntL n _) = text (show n)
@@ -26,12 +53,11 @@ pprSpecificity (Decond mono_type spcs) =
   let type_doc =
         case mono_type
         of VarDeCon con ty_args ex_types ->
-             let types_doc = [pprTypePrec t ?+ appPrec | t <- ty_args]
-                 ex_doc    = [ parens $ pprVar v <+> text ":" <+> pprType t
-                             | (v ::: t) <- ex_types]
+             let types_doc = pprTyArgs ty_args
+                 ex_doc    = pprExTypeBinders ex_types
              in parens $ sep (pprVar con : types_doc ++ ex_doc)
            TupleDeCon ty_args ->
-             let types_doc = [pprTypePrec t ?+ appPrec | t <- ty_args]
+             let types_doc = [pprType t | t <- ty_args]
              in parens $ sep $ punctuate (text ",") types_doc
   in text "D" <> braces (type_doc <> text ":" <> cat (map pprSpecificity spcs))
 
@@ -41,25 +67,6 @@ pprSpecificity (Read (HeapMap m)) =
   where
     cells = [text "()" <+> text "|->" <+> pprSpecificity s | (v, s) <- m]
 pprSpecificity Unused = text "0"
-{-
-pprParamRepr repr =
-  let repr_word = pprParamReprWord repr
-  in case repr
-     of ValPT (Just v) -> repr_word <> braces (pprVar v) 
-        _ -> repr_word
-
-pprParamType :: ParamType -> Doc
-pprParamType pt = unparenthesized $ pprParamTypePrec pt
-
-pprParamTypePrec :: ParamType -> PrecDoc
-pprParamTypePrec (repr ::: ty) =
-  pprParamRepr repr <+> pprType ty `hasPrec` appPrec
-
-pprReturnType rt = unparenthesized $ pprReturnTypePrec rt
-
-pprReturnTypePrec (repr ::: ty) =
-  pprReturnRepr repr <+> pprType ty `hasPrec` appPrec
--}
 
 pprTypM (TypM t) = pprType t
 
@@ -79,10 +86,10 @@ pprExpPrec (ExpM expression) =
      LitE _ l -> hasAtomicPrec $ pprLit l
      ConE _ (CInstM (VarCon op ty_args ex_types)) args ->
        let op_doc = text "<" <> pprVar op <> text ">"
-           ty_args_doc = [text "@" <> pprTypePrec t ?+ appPrec | t <- ty_args]
-           ex_types_doc = [text "&" <> pprTypePrec t ?+ appPrec | t <- ex_types]
+           ty_args_doc = pprTyArgs ty_args
+           ex_types_doc = pprExTypeArgs ex_types
            args_doc = [pprExpPrec arg ?+ appPrec | arg <- args]
-       in hang op_doc 6 (sep $ ty_args_doc ++ ex_types_doc ++ args_doc)
+       in hang op_doc appIndent (sep $ ty_args_doc ++ ex_types_doc ++ args_doc)
           `hasPrec` appPrec
 
      ConE _ (CInstM (TupleCon _)) args ->
@@ -92,14 +99,14 @@ pprExpPrec (ExpM expression) =
        let op_doc = pprExpPrec op ?+ appPrec
            ty_args_doc = [text "@" <> pprTypePrec t ?+ appPrec | TypM t <- ty_args]
            args_doc = [pprExpPrec arg ?+ appPrec | arg <- args]
-       in hang op_doc 6 (sep $ ty_args_doc ++ args_doc) `hasPrec` appPrec
+       in hang op_doc appIndent (sep $ ty_args_doc ++ args_doc) `hasPrec` appPrec
 
      LamE _ f -> pprFunPrec f
      LetE _ pat rhs body ->
        let pat_doc = pprPat pat
            rhs_doc = pprExpPrec rhs ?+ outerPrec
            body_doc = pprExpPrec body ?+ outerPrec
-       in hang (pat_doc <+> text "=") 4 rhs_doc $$ body_doc `hasPrec` stmtPrec
+       in hang (pat_doc <+> text "=") letIndent rhs_doc $$ body_doc `hasPrec` stmtPrec
      LetfunE _ defs body ->
        let defs_doc = pprDefGroup defs
            body_doc = pprExp body
@@ -109,7 +116,7 @@ pprExpPrec (ExpM expression) =
        let binder_doc = text "let" <+> pprPatternMatch altm <+> text "="
            scr_doc = pprExp scr
            body_doc = pprExp (altBody alt)
-       in hang binder_doc 8 scr_doc $$ body_doc `hasPrec` stmtPrec
+       in hang binder_doc caseIndent scr_doc $$ body_doc `hasPrec` stmtPrec
      CaseE _ scr alts ->
        let case_doc = text "case" <+> pprExpPrec scr ? stmtPrec 
            of_doc = text "of" <+> vcat (map pprAlt alts)
@@ -119,23 +126,22 @@ pprExpPrec (ExpM expression) =
      CoerceE _ from_t to_t body ->
        let coercion_doc = pprType (fromTypM from_t) <+> text "=>" <+> pprType (fromTypM to_t)
            b_doc = pprExp body
-       in hang (text "coerce" <+> parens coercion_doc) 4 b_doc `hasPrec` appPrec
+       in hang (text "coerce" <+> parens coercion_doc) letIndent b_doc `hasPrec` appPrec
 
 
 pprPatternMatch (AltM (Alt (DeCInstM (VarDeCon con ty_args ex_types)) params _)) =
   let con_doc = pprVar con
-      args_doc = pprParenList $ map pprType ty_args
-      ex_types_doc = [parens $ pprVar v <+> text ":" <+> pprType t
-                     | v ::: t <- ex_types]
+      args_doc = pprTyArgs ty_args
+      ex_types_doc = pprExTypeBinders ex_types
       params_doc = map (parens . pprPat) params
-  in con_doc <+> sep (args_doc : ex_types_doc ++ params_doc)
+  in hang con_doc appIndent (sep $ args_doc ++ ex_types_doc ++ params_doc)
 
 pprPatternMatch (AltM (Alt (DeCInstM (TupleDeCon _)) params _)) =
   pprParenList (map pprPat params)
 
 pprAlt altm@(AltM alt) =
-  pprPatternMatch altm <> text "." $$ 
-  nest 2 (pprExp $ altBody alt)
+  hang (pprPatternMatch altm <> text ".") defIndent $
+  pprExp (altBody alt)
 
 pprFun f = unparenthesized $ pprFunPrec f
 
@@ -146,10 +152,12 @@ pprFunPrec (FunM fun) =
       body_doc = pprExpPrec (funBody fun) ? stmtPrec
       sig_doc = sep [pprParenList (ty_params_doc ++ params_doc),
                      nest (-3) $ text "->" <+> return_doc]
-  in text "lambda" <+> sig_doc <> text "." $$ nest 4 body_doc
+  in hang (text "lambda" <+> sig_doc <> text ".") defIndent body_doc
      `hasPrec` stmtPrec
 
-pprDef (Def v ann f) = hang (pprVar v <+> ann_doc <+> text "=") 2 (pprFun f)
+pprDef (Def v ann f) =
+  hang (pprVar v <+> ann_doc <+> text "=") defIndent $
+  pprFun f
   where
     ann_doc =
       let phase_doc =
