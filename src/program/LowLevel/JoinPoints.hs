48f96139182499d05b8eb674839a91b25181c5eb
cirodrig <cirodrig@illinois.edu>
2011-10-20 18:02:05 -0500

    New join point finding algorithm
    Ignore-this: 556d2710a18cb607a28d5ba7beea777
    The old join point algorithm was too aggressive, and would
    label functions as join points even if they were not inside
    the caller.  As a result, inlining would erase code.
    
    darcs-hash:20111020230205-11156-b294f36f2c6d7fa5bbb87a1bea0217b780f79654.gz

--- src/program/LowLevel/JoinPoints.hs
+++ src/program/LowLevel/JoinPoints.hs
@@ -12,194 +12,245 @@ uses knowledge of join points to keep tail calls of local functions in tail
 position.
 
 One top-level function at a time is converted.  The code is scanned to build a
-/tail-calls/ graph in which an edge @f -> g@ means that @f@ tail-calls @g@
-and @g@ is not referenced in a way other than a tail call.  The roots of the
-graph, that is, functions that are used in some way other than a tail call,
-are also identified.  Reachability from roots is determined using depth-first
-search.  If any node is reachable from multiple roots, it is not a join point;
-the remaining reachable set are endpoints.
+/tail-calls/ graph in which a node represents a local or top-level function
+and an edge @f -> g@ means that @f@ tail-calls @g@.
+Each node is labeled with a list of /parents/.  The parents of a function
+@f@ are the functions whose body contains the definition of @f@ somewhere
+inside.
+Each node is also labeled with a /root/, which is its unique non-join-point
+caller.  The initial set of roots is determined optimistically.  Any function
+that is used in some way other than a saturated tail call is a root, and the
+top-level function is a root.
+
+The analysis turns additional functions into roots if they have any of several
+properties.  A function becomes a root if it has two non-join-point callers.
+A function becomes a root if its would-be root is not a parent.
 -}
 
 module LowLevel.JoinPoints(convertJoinPoints) where
 
+import Control.Monad
+import qualified Data.HashTable as HashTable
 import Data.Graph.Inductive(Gr)
 import Data.Graph.Inductive.Graph
 import Data.Graph.Inductive.Query.DFS
+import Data.IORef
 import qualified Data.IntMap as IntMap
 import Data.List as List
 import qualified Data.Map as Map
+import Data.Maybe
 import Data.Monoid
 import qualified Data.Set as Set
+import Text.PrettyPrint.HughesPJ
 
 import Common.Error
 import Common.Identifier
+import Common.Worklist
 import LowLevel.CodeTypes
 import LowLevel.FreshVar
 import LowLevel.Rename
 import LowLevel.Syntax
+import LowLevel.Print
 import Globals
 
 -------------------------------------------------------------------------------
--- Finding join points
+-- Finding join points.
 
--- | Information about observed tail calls observed in a function.
---
---   This keeps track of roots, functions that are defined in the scanned
---   scope, and tail calls.
-data ObservedTailCalls = ObservedTailCalls [Var] [Var] [(Var, Var)]
+-- A top-level function is first scanned to build an 'AnalysisTable'
+-- containing information about all local functions.  Then all local functions 
+-- that do /not/ satisfy the criteria for being join points are identified.
+-- The list of such functions is returned.
 
-instance Monoid ObservedTailCalls where
-  mempty = ObservedTailCalls [] [] []
-  ObservedTailCalls x1 y1 z1 `mappend` ObservedTailCalls x2 y2 z2 =
-    ObservedTailCalls (x1 ++ x2) (y1 ++ y2) (z1 ++ z2)
+-- | A list of a function's parents.  @f@ is a parent of @g@ if the
+--   definition of @f@ contains the definition of @g@.
+type Parents = [Var]
 
--- | A tail call scan.  The scan needs to know the function in which
---   the current code is contained and the arity of all locally defined
---   functions.
-type TailCallScan = Var -> IntMap.IntMap Int -> ObservedTailCalls
+-- | Analysis data associated with a function.
+data AnalysisData =
+  AnalysisData
+  { -- | The root currently assigned to the function.
+    --   A function is a root iff it is its own root.
+    --   'Nothing' if no root has been assigned.
+    rootRef :: {-# UNPACK #-}!(IORef (Maybe Var))
 
-setCallingContext :: Var -> TailCallScan -> TailCallScan
-setCallingContext v f = \_ arities -> f v arities
+    -- | Arity of this function
+  , arity :: {-# UNPACK #-}!Int
 
--- | Scan code that may use a definition of a function.  The function's
---   arity is given as a parameter.
-withDefinition :: Var -> Int -> TailCallScan -> TailCallScan
-withDefinition v arity f = \cc arities -> 
-  let arities' = IntMap.insert (fromIdent $ varID v) arity arities
-      ObservedTailCalls roots defs edges = f cc arities'
-  in ObservedTailCalls roots (v:defs) edges
+    -- | Functions that are tail called by this function.
+    --   This IORef is updated when building the hash table.
+    --   By the time solving begins, all callee lists are immutable.
+  , tailCallees :: {-# UNPACK #-}!(IORef [Var])
 
--- | Use a variable in a non-tail-call context
-use :: Var -> TailCallScan
-use v = scanner
-  where
-    scanner _ arities
-      | fromIdent (varID v) `IntMap.member` arities =
-          ObservedTailCalls [v] [] []
-      | otherwise = mempty
+    -- | The function definitions containing the definition of this function
+  , parents :: !Parents
+  }
 
--- | Use a variable in a tail call
-useTailCall :: Var -> Int -> TailCallScan
-useTailCall v num_args = scanner
-  where
-    scanner caller arities =
-      case IntMap.lookup (fromIdent $ varID v) arities
-      of Nothing -> mempty      -- Variable is ignored 
-         Just arity | arity /= num_args -> non_tail_call
-                    | otherwise         -> tail_call caller v
-    non_tail_call = ObservedTailCalls [v] [] []
-    tail_call caller callee = ObservedTailCalls [] [] [(caller, callee)]
+isRootData :: Var -> AnalysisData -> IO Bool
+isRootData v d = do
+  root <- readIORef $ rootRef d
+  return $ root == Just v
 
-tailCallScanVal :: Val -> TailCallScan
-tailCallScanVal value =
-  case value
-  of VarV v -> use v
-     RecV _ vs -> tailCallScanVals vs
-     LitV _ -> mempty
-     
-tailCallScanVals :: [Val] -> TailCallScan
-tailCallScanVals vs = mconcat $ map tailCallScanVal vs
+type AnalysisTable = HashTable.HashTable Var AnalysisData
 
--- | Scan an atom that's not in tail position
-tailCallScanAtom :: Atom -> TailCallScan
-tailCallScanAtom (ValA vs) = tailCallScanVals vs
-tailCallScanAtom (CallA _ op args) = tailCallScanVals (op:args)
-tailCallScanAtom (PrimA _ vs) = tailCallScanVals vs
-tailCallScanAtom (PackA _ vs) = tailCallScanVals vs
-tailCallScanAtom (UnpackA _ v) = tailCallScanVal v
+lookupFun :: AnalysisTable -> Var -> IO (Maybe AnalysisData)
+lookupFun = HashTable.lookup
 
-tailCallScanStm :: Stm -> TailCallScan
-tailCallScanStm statement =
+lookupFun' :: AnalysisTable -> Var -> IO AnalysisData
+lookupFun' tbl v = do
+  md <- lookupFun tbl v
+  case md of
+    Just d -> return d
+    Nothing -> internalError "lookupFun'"
+
+-- | Find the hash table members that are roots
+getRoots :: AnalysisTable -> IO [Var]
+getRoots tbl = do
+  members <- HashTable.toList tbl
+  roots <- filterM (uncurry isRootData) members
+  return $ map fst roots
+
+-- | Make the given variable a root, if it's in the table.
+makeRoot :: AnalysisTable -> Var -> IO ()
+makeRoot tbl v = do
+  md <- lookupFun tbl v
+  case md of
+    Nothing -> return ()
+    Just d  -> writeIORef (rootRef d) (Just v)
+
+createAnalysisTable :: FunDef -> IO AnalysisTable
+createAnalysisTable def = do
+  tbl <- HashTable.new (==) (HashTable.hashInt . fromIdent . varID)
+  scanDef tbl [] def
+  makeRoot tbl (definiendum def) -- The top-level function is a root
+  return tbl
+
+-- | Create an entry for a function definition
+setupDef tbl parent_list (Def v f) = do
+  root_ref <- newIORef Nothing
+  callees_ref <- newIORef []
+  HashTable.insert tbl v $ AnalysisData { rootRef = root_ref 
+                                        , arity = length $ funParams f
+                                        , tailCallees = callees_ref 
+                                        , parents = parent_list}
+  return callees_ref
+
+-- | Scan a function definition
+scanDefBody tbl parent_list callees_ref (Def v f) =
+  scanStm tbl callees_ref (v : parent_list) (funBody f)
+
+scanDef :: AnalysisTable -> Parents -> FunDef -> IO ()
+scanDef tbl parent_list def = do
+  callees_ref <- setupDef tbl parent_list def
+  scanDefBody tbl parent_list callees_ref def
+
+scanDefs :: AnalysisTable -> Parents -> Group FunDef -> IO ()
+scanDefs tbl parent_list defs = do
+  callees <- mapM (setupDef tbl parent_list) (groupMembers defs)
+  zipWithM_ (scanDefBody tbl parent_list) callees (groupMembers defs)
+
+scanStm tbl callees_ref parent_list statement =
   case statement
-  of LetE params rhs body ->
-       tailCallScanAtom rhs `mappend` tailCallScanStm body
-     LetrecE defs body ->
-       tailCallScanDefs defs $ tailCallScanStm body
-     SwitchE scrutinee alternatives ->
-       mconcat $
-       tailCallScanVal scrutinee : map (tailCallScanStm . snd) alternatives
-
-     -- Scan a tail call
-     ReturnE (CallA _ (VarV op) args) ->
-       useTailCall op (length args) `mappend` tailCallScanVals args
+  of LetE _ rhs body -> do
+       scanAtom tbl rhs
+       continue body
+     LetrecE defs body -> do
+       scanDefs tbl parent_list defs
+       continue body
+     SwitchE scrutinee alts -> do
+       scanVal tbl scrutinee
+       sequence_ [continue s | (_, s) <- alts]
+     ReturnE (CallA _ (VarV op) args) -> do
+       -- tail call
+       scanTailCall tbl callees_ref op (length args)
+       scanVals tbl args
      ReturnE atom ->
-       tailCallScanAtom atom
-     ThrowE val ->
-       tailCallScanVal val
-
-tailCallScanFun :: Fun -> TailCallScan
-tailCallScanFun f = tailCallScanStm (funBody f)
-
-tailCallScanDefs defs scan = with_definitions (scan `mappend` scan_definitions)
+       scanAtom tbl atom
+     ThrowE v ->
+       scanVal tbl v
   where
-    with_definitions k = foldr with_definition k (groupMembers defs)
-    with_definition def k =
-      withDefinition (definiendum def) (length $ funParams $ definiens def) k
-    
-    scan_definitions =
-      mconcat [setCallingContext fname $ tailCallScanFun fun
-              | Def fname fun <- groupMembers defs]
+    continue s = scanStm tbl callees_ref parent_list s
 
--- | Scan a top-level function.  Indicate that the function is used outside of
---   the scanned code.
-tailCallScanTopLevelFunction :: FunDef -> TailCallScan
-tailCallScanTopLevelFunction def =
-  tailCallScanDefs (Rec [def]) (use $ definiendum def)
+-- | Scan a tail call.  If the callee is in the table and the call has
+--   the correct number of arguments, record the fact
+--   that the current function has a tail call by modifying @callees_ref@.
+scanTailCall tbl callees_ref op n_args = do
+  md <- lookupFun tbl op
+  case md of
+    Just d | n_args == arity d -> modifyIORef callees_ref (insert op)
+    _                          -> return ()
+  
+scanAtom tbl atom =
+  case atom
+  of ValA vs -> scanVals tbl vs
+     CallA _ v vs -> scanVal tbl v >> scanVals tbl vs
+     PrimA _ vs -> scanVals tbl vs
+     PackA _ vs -> scanVals tbl vs
+     UnpackA _ v -> scanVal tbl v
 
--- | Create the tail-call graph of a top-level function.
--- Return the set of root nodes and the graph.
-mkTailCallGraph :: FunDef -> ([Node], Gr Var ())
-mkTailCallGraph fdef = 
-  let ObservedTailCalls roots defs edges =
-        tailCallScanTopLevelFunction fdef no_context IntMap.empty
-        where
-          no_context = internalError "mkTailCallGraph: No calling context"
-      
-      -- Get the set of roots and the set of all defined functions.
-      root_set = Set.fromList roots
-      all_nodes = Set.fromList defs -- Making a set removes duplicates
-      node_map :: Map.Map Var Node
-      node_map = Map.fromList $ zip (Set.toList all_nodes) [1..]
-      root_nodes = [node_map Map.! v | v <- Set.toList root_set]
+scanVals tbl vs = mapM_ (scanVal tbl) vs
 
-      graph_edges =
-        [(node_map Map.! s, node_map Map.! d, ())
-        | (s, d) <- edges, not $ d `Set.member` root_set]
-      graph_nodes = [(n, l) | (l, n) <- Map.assocs node_map]
-      graph = mkGraph graph_nodes graph_edges
-  in (root_nodes, graph)
+scanVal tbl value = 
+  case value
+  of VarV v -> makeRoot tbl v
+     RecV _ vs -> scanVals tbl vs
+     LitV _ -> return ()
 
--- | For each root node of the tail-call graph, list its join points
-type JoinPoints = [(Var, [Var])]
+solveJoinPoints tbl wl =
+  -- Iterate until worklist is empty
+  forWorklist wl $ \f -> do
+    f_info <- lookupFun' tbl f
 
-getJoinPoints :: FunDef -> JoinPoints
-getJoinPoints fdef =
-  let -- Get the tail call graph
-      (roots, gr) = mkTailCallGraph fdef
+    -- For each function that is tail-called by f
+    callees <- readIORef $ tailCallees f_info
+    forM_ callees $ \callee -> do
+      callee_info <- lookupFun' tbl callee
 
-      -- Start with the reachability graph
-      endpoints = [(root, reachable root gr) | root <- roots]
+      -- Look up the root currently assigned to 'f'.  Something must
+      -- have been assigned before 'f' was put into the worklist.
+      -- This lookup is in the inner loop because the root of 'f' may be
+      -- modified by the inner loop.
+      m_f_root <- readIORef $ rootRef f_info
+      f_root <- case m_f_root
+                of Nothing -> internalError "solveJoinPoints"
+                   Just rt -> return rt
 
-      -- If a node is reachable from multiple roots, remove it and make it its
-      -- own endpoint
-      multiple_endpoints = [n | n <- nodes gr, memberN 2 n (map snd endpoints)]
-      f_endpoints = [(n, [n]) | n <- multiple_endpoints] ++
-                    [(n, ns List.\\ multiple_endpoints) | (n, ns) <- endpoints]
+      -- The callee may only be a join point if it is defined inside 
+      -- the root of f.  Otherwise it will beome a root.
+      let new_root = if f_root `elem` parents callee_info
+                     then f_root
+                     else callee
+      updateRoot wl callee callee_info new_root
 
-      getlab node = case lab gr node
-                    of Just l -> l
-                       Nothing -> internalError "getJoinPoints"
+-- | Update a function's root
+updateRoot :: Worklist Var -> Var -> AnalysisData -> Var -> IO ()
+updateRoot wl callee callee_info new_root = do
+  -- Assign the callee's root.  If the new root and old root disagree, 
+  -- then the callee becomes a root.
+  let update_root Nothing      = Just new_root
+      update_root (Just old_root) 
+        | old_root == new_root = Just new_root
+        | otherwise            = Just callee -- Callee becomes a root
+  changed <- modifyCheckIORef update_root $ rootRef callee_info
+  when changed $ putWorklist wl callee
 
-      -- Remove a node from its own reachability list.  By definition, a
-      -- function is not is own join point.
-      join_points = [(n, delete n ns) | (n, ns) <- f_endpoints]
-  in [(getlab n, map getlab e) | (n, e) <- join_points]
-  where
-    -- memberN n x xss -> True if x appears in at least n members of xss 
-    memberN 0 _ _        = True
-    memberN _ _ []       = False
-    memberN n x (xs:xss) = let n' = if x `elem` xs then n - 1 else n
-                           in memberN n' x xss
+-- | The main analysis routine.  Given a top-level function definition, this
+--   analysis returns the list of join points that are defined in the function.
+getJoinPoints :: FunDef -> IO [Var]
+getJoinPoints top_level_def = do
+  -- Setup
+  tbl <- createAnalysisTable top_level_def
+  roots <- getRoots tbl
+  wl <- newWorklist roots
+
+  -- Computation
+  solveJoinPoints tbl wl
+
+  -- Read the set of join points
+  let append_root_entry root_list (v, d) = do
+        is_root <- isRootData v d
+        return $! if is_root then root_list else v : root_list
+  entries <- HashTable.toList tbl
+  foldM append_root_entry [] entries
 
 -------------------------------------------------------------------------------
 
@@ -246,17 +297,22 @@ labelStm join_points s =
 -------------------------------------------------------------------------------
 
 -- | Find and annotate join points in one top-level function
-convertJoinPointsDef :: FunDef -> FunDef
-convertJoinPointsDef def =
-  let join_points_list = getJoinPoints def
-      join_points_set = Set.fromList $ concatMap snd join_points_list
-  in labelFunDef join_points_set def
+convertJoinPointsDef :: FunDef -> IO FunDef
+convertJoinPointsDef def = do
+  join_points_list <- getJoinPoints def
+  let join_points_set = Set.fromList join_points_list
+  return $ labelFunDef join_points_set def
 
-convertJoinPointsGroup :: Group GlobalDef -> Group GlobalDef
-convertJoinPointsGroup grp = fmap convert_def grp
+convertJoinPointsGroup :: Group GlobalDef -> IO (Group GlobalDef)
+convertJoinPointsGroup grp =
+  case grp
+  of NonRec def -> NonRec `liftM` convert_def def
+     Rec defs -> Rec `liftM` mapM convert_def defs
   where
-    convert_def (GlobalFunDef fdef) = GlobalFunDef (convertJoinPointsDef fdef)
-    convert_def (GlobalDataDef ddef) = GlobalDataDef ddef
+    convert_def (GlobalFunDef fdef) =
+      GlobalFunDef `liftM` convertJoinPointsDef fdef
+    convert_def (GlobalDataDef ddef) =
+      return $ GlobalDataDef ddef
 
 convertJoinPoints :: Module -> IO Module
 convertJoinPoints mod = do
@@ -265,4 +321,5 @@ convertJoinPoints mod = do
     runFreshVarM var_ids $ renameModule RenameFunctions emptyRenaming mod
   
   -- Convert all join points
-  return $ mod {moduleGlobals = map convertJoinPointsGroup $ moduleGlobals mod}
+  globals <- mapM convertJoinPointsGroup $ moduleGlobals mod
+  return $ mod {moduleGlobals = globals}
